0a5d457b6d3be8940d6f6af754646173568b4df3
johantodd <joahntodd@tutanota.de>
2018-01-28 23:40:28 +0200

    commit SAI

---
+++ DS/upload_DS.sh
@@ -0,0 +1,5 @@
+git remote add origin https://github.com/johantodd/Docs.git
+git add *
+git status
+git commit -m "commit DS"
+git push -u origin master
---
+++ SAI/upload_SAI.sh
@@ -0,0 +1,5 @@
+git remote add origin https://github.com/johantodd/Docs.git
+git add *
+git status
+git commit -m "commit SAI"
+git push -u origin master
